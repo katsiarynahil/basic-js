f142de1598fd1380455c0ae4399b2279ac5e6b80
Katsiaryna Hil <khil@mobileiron.com>
2020-12-14 00:51:57 +0300

    add recursive-depth and vigenere-cipher solutions

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,6 +1,17 @@
 
-module.exports = class DepthCalculator {  calculateDepth(arr) {
-    
-    
+module.exports = class DepthCalculator {  
+  
+  calculateDepth(arr) {
+
+    let depth = 1
+    let depthArray = []
+
+    let onlyArrays = arr.filter(element => Array.isArray(element));
+
+    for(let innerArr of onlyArrays){
+      let innerDepth = this.calculateDepth(innerArr);
+      depthArray.push(innerDepth);
+    }
+    return depthArray.length === 0 ? depth : Math.max(...depthArray) + depth;
   }
 };
